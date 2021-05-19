0320824a626783e95b128c3a08c68fb4923c59e6
ServicePorta <76655416+ServicePorta@users.noreply.github.com>
2021-05-19 07:09:56 +0530

    End of section 6

--- client-app/src/features/activities/dashboard/form/ActivityForm.tsx
+++ client-app/src/features/activities/dashboard/form/ActivityForm.tsx
@@ -1,14 +1,16 @@
 import React, { ChangeEvent, useState } from "react";
 import { Button, Form, Segment } from "semantic-ui-react";
+import LoadingComponent from "../../../../app/layout/LoadingComponent";
 import { Activity } from "../../../../app/models/activity";
 
 interface Props {
   closeForm: () => void;
   activity: Activity | undefined;
   createOrEditActivity: (activity: Activity) => void;
+  submitting:boolean;
 }
 
-const ActivityForm = ({ closeForm, activity: selectedActivity,createOrEditActivity }: Props) => {
+const ActivityForm = ({ closeForm, activity: selectedActivity,createOrEditActivity,submitting }: Props) => {
   const initialState = selectedActivity ?? {
     id: "",
     title: "",
@@ -30,16 +32,18 @@ const ActivityForm = ({ closeForm, activity: selectedActivity,createOrEditActivi
     setActivity({...activity,[name]:value})
   };
 
+  // if (submitting) return <LoadingComponent content={"Loading Activities..."} />;
+
   return (
     <Segment clearing>
       <Form onSubmit={handleSubmit} autoComplete="off">
         <Form.Input placeholder="Title" name="title" value={activity.title} onChange={handleInputChange}  />
         <Form.TextArea placeholder="Description" name='description' value={activity.description} onChange={handleInputChange}   />
         <Form.Input placeholder="Category" name='category' value={activity.category} onChange={handleInputChange}   />
-        <Form.Input placeholder="Date" name='date' value={activity.date} onChange={handleInputChange}   />
+        <Form.Input type='date' placeholder="Date" name='date' value={activity.date} onChange={handleInputChange}   />
         <Form.Input placeholder="City" name='city' value={activity.city} onChange={handleInputChange}   />
         <Form.Input placeholder="Venue" name='venue' value={activity.venue} onChange={handleInputChange}   />
-        <Button floated="right" positive type="submit" content="Submit" />
+        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
           floated="right"
           type="button"
